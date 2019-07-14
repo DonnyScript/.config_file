a4fe66ac53cb57eaabeff9ae93827f901cfce282
DonnyScript <DonnyScript@gmail.com>
2019-07-14 06:57:19 -0400

    commit

---
+++ .config/cmus/lib.pl
@@ -0,0 +1,5 @@
+/home/donny/Downloads/Bitches Brew.mp3
+/home/donny/Music/02 - Bitches Brew.mp3
+/home/donny/Music/Bitches_Brew.ogg
+/home/donny/Music/Jazz /02 - Bitches Brew.mp3
+/home/donny/Music/Jazz /Bitches_Brew.ogg
